e9f7a7ac5c81e9b0ddb9d6450c20934a3b31b91d
John Velis <john@johnvelis.com>
2018-01-19 14:26:44 -0500

    S1_Starter2 added

---
+++ Demo_TheTravelingSalesperson.S1_Starter2/Controllers/Controller.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_TheTravelingSalesperson
+{
+    /// <summary>
+    /// MVC Controller class
+    /// </summary>
+    public class Controller
+    {
+        #region FIELDS
+
+        private bool _usingApplication;
+
+        //
+        // declare ConsoleView and Salesperson objects for the Controller to use
+        // Note: There is no need for a Salesperson or ConsoleView property given only the Controller 
+        //       will access the ConsoleView object and will pass the Salesperson object to the ConsoleView.
+        //
+
+
+        #endregion
+
+        #region PROPERTIES
+
+
+        #endregion
+        
+        #region CONSTRUCTORS
+
+        public Controller()
+        {
+            InitializeController();
+
+            //
+            // instantiate a Salesperson object
+            //
+
+
+            //
+            // instantiate a ConsoleView object
+            //
+
+
+            //
+            // begins running the application UI
+            //
+            ManageApplicationLoop();
+        }
+
+        #endregion
+        
+        #region METHODS
+
+        /// <summary>
+        /// initialize the controller 
+        /// </summary>
+        private void InitializeController()
+        {
+            _usingApplication = true;
+        }
+
+        /// <summary>
+        /// method to manage the application setup and control loop
+        /// </summary>
+        private void ManageApplicationLoop()
+        {
+
+            //
+            // close the application
+            //
+            Environment.Exit(1);
+        }
+
+        /// <summary>
+        /// add the next city location to the list of cities
+        /// </summary>
+        private void Travel()
+        {
+
+        }
+
+        /// <summary>
+        /// display all cities traveled to
+        /// </summary>
+        private void DisplayCities()
+        {
+
+        }
+
+        /// <summary>
+        /// display account information
+        /// </summary>
+        private void DisplayAccountInfo()
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ Demo_TheTravelingSalesperson.S1_Starter2/Views/ConsoleView.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_TheTravelingSalesperson
+{
+    /// <summary>
+    /// MVC View class
+    /// </summary>
+    public class ConsoleView
+    {
+        #region FIELDS
+
+        #endregion
+
+        #region PROPERTIES
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        /// <summary>
+        /// default constructor to create the console view objects
+        /// </summary>
+        public ConsoleView()
+        {
+            InitializeConsole();
+        }
+
+        #endregion
+
+        #region METHODS
+
+        /// <summary>
+        /// initialize all console settings
+        /// </summary>
+        private void InitializeConsole()
+        {
+            ConsoleUtil.WindowTitle = "Laughing Leaf Productions";
+            ConsoleUtil.HeaderText = "The Traveling Salesperson Application";
+        }
+
+        /// <summary>
+        /// display the Continue prompt
+        /// </summary>
+        public void DisplayContinuePrompt()
+        {
+            Console.CursorVisible = false;
+
+            ConsoleUtil.DisplayMessage("");
+
+            ConsoleUtil.DisplayMessage("Press any key to continue.");
+            ConsoleKeyInfo response = Console.ReadKey();
+
+            ConsoleUtil.DisplayMessage("");
+
+            Console.CursorVisible = true;
+        }
+
+        /// <summary>
+        /// display the Exit prompt on a clean screen
+        /// </summary>
+        public void DisplayExitPrompt()
+        {
+            ConsoleUtil.DisplayReset();
+
+            Console.CursorVisible = false;
+
+            ConsoleUtil.DisplayMessage("");
+            ConsoleUtil.DisplayMessage("Thank you for using the application. Press any key to Exit.");
+
+            Console.ReadKey();
+
+            System.Environment.Exit(1);
+        }
+
+
+        /// <summary>
+        /// display the welcome screen
+        /// </summary>
+        public void DisplayWelcomeScreen()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            ConsoleUtil.DisplayReset();
+
+            ConsoleUtil.DisplayMessage("Written by John Velis");
+            ConsoleUtil.DisplayMessage("Northwestern Michigan College");
+            ConsoleUtil.DisplayMessage("");
+
+            sb.Clear();
+            sb.AppendFormat("You are a traveling salesperson buying and selling widgets ");
+            sb.AppendFormat("around the country. You will be prompted regarding which city ");
+            sb.AppendFormat("you wish to travel to and will then be asked whether you wish to buy ");
+            sb.AppendFormat("or sell widgets.");
+            ConsoleUtil.DisplayMessage(sb.ToString());
+            ConsoleUtil.DisplayMessage("");
+
+            sb.Clear();
+            sb.AppendFormat("Your first task will be to set up your account details.");
+            ConsoleUtil.DisplayMessage(sb.ToString());
+
+            DisplayContinuePrompt();
+        }
+
+        /// <summary>
+        /// setup the new salesperson object with the initial data
+        /// Note: To maintain the pattern of only the Controller changing the data this method should
+        ///       return a Salesperson object with the initial data to the controller. For simplicity in 
+        ///       this demo, the ConsoleView object is allowed to access the Salesperson object's properties.
+        /// </summary>
+        public Salesperson DisplaySetupAccount()
+        {
+            Salesperson salesperson = new Salesperson();
+
+            return salesperson;
+        }
+
+        /// <summary>
+        /// display a closing screen when the user quits the application
+        /// </summary>
+        public void DisplayClosingScreen()
+        {
+            ConsoleUtil.DisplayReset();
+
+            ConsoleUtil.DisplayMessage("Thank you for using The Traveling Salesperson Application.");
+
+            DisplayContinuePrompt();
+        }
+
+        /// <summary>
+        /// get the menu choice from the user
+        /// </summary>
+        public MenuOption DisplayGetUserMenuChoice()
+        {
+            MenuOption userMenuChoice = MenuOption.None;
+
+            return userMenuChoice;
+        }
+        /// <summary>
+        /// get the next city to travel to from the user
+        /// </summary>
+        /// <returns>string City</returns>
+        public string DisplayGetNextCity()
+        {
+            string nextCity = "";
+
+            return nextCity;
+        }
+
+        /// <summary>
+        /// display a list of the cities traveled
+        /// </summary>
+        public void DisplayCitiesTraveled(Salesperson salesperson)
+        {
+
+        }
+
+        /// <summary>
+        /// display the current account information
+        /// </summary>
+        public void DisplayAccountInfo(Salesperson salesperson)
+        {
+
+        }
+
+        #endregion
+    }
+}
